0dcffd26856de0078e808072cc68f88427140ace
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2015-03-22 14:05:23 -0700

    Initial spotify bakcup app commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyBackupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spotifyBackupApp
+ */
+angular.module('spotifyBackupApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyBackupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spotifyBackupApp
+ */
+angular.module('spotifyBackupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+	'TEST'
+    ];
+  });
